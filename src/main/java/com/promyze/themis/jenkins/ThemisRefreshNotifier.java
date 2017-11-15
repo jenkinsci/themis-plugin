d5788a4ec5358c0c1e08eba5039f1e18940cc205
Romain Delamare <romain.delamare@promyze.com>
2017-11-15 09:59:44 +0100

    Add actual refresh behavior to ThemisRefreshNotifier

--- src/main/java/com/promyze/themis/jenkins/ThemisRefreshNotifier.java
+++ src/main/java/com/promyze/themis/jenkins/ThemisRefreshNotifier.java
@@ -1,5 +1,9 @@
 package com.promyze.themis.jenkins;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.promyze.themis.jenkins.ThemisGlobalConfiguration.ThemisInstance;
 import hudson.AbortException;
 import hudson.Extension;
@@ -18,12 +22,18 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.inject.Inject;
+import java.io.PrintStream;
+import java.text.MessageFormat;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Sample notifier class
  */
 public class ThemisRefreshNotifier extends Notifier {
+
+    private static final String REFRESH_URL_FORMAT = "{0}/api/refreshProject/{1}";
+    private static final String THEMIS_API_KEY = "themis-api-key";
 
     private String instanceName;
     private String projectKey;
@@ -55,20 +65,42 @@ public class ThemisRefreshNotifier extends Notifier {
 
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws AbortException {
+        PrintStream logger = listener.getLogger();
         ThemisInstance instance = GlobalConfiguration.all().get(ThemisGlobalConfiguration.class)
                 .getInstance(instanceName);
         if (instance == null) {
-            if (failBuild) {
-                throw new AbortException(Messages.unknownInstance(instanceName));
-            } else {
-                listener.getLogger().println(Messages.unknownInstance(instanceName));
-                return true;
-            }
+            return fail(logger, Messages.unknownInstance(instanceName));
         }
-        // TODO actually refresh Themis
-        listener.getLogger().println(String.format("Refreshing Themis: %s?apiKey=%s&projectKey=%s",
-                                                   instance.getUrl(), instance.getApiKey(), projectKey));
+
+        try {
+            HttpResponse<String> response = refreshThemis(instance);
+            if (response.getStatus() == 200) {
+                JsonNode node = new JsonNode(response.getBody());
+                logger.println(Messages.projectRefreshed(node.getObject().get("dataDisplayed")));
+            } else {
+                return fail(logger, Messages.refreshError(response.getStatus(), response.getBody()));
+            }
+        } catch (UnirestException e) {
+            return fail(logger, Messages.refreshUnknownError(e.getMessage()));
+        }
+
         return true;
+    }
+
+    private HttpResponse<String> refreshThemis(ThemisInstance instance) throws UnirestException {
+        String url = MessageFormat.format(REFRESH_URL_FORMAT, instance.getUrl(), projectKey);
+        return Unirest.get(url)
+                .header(THEMIS_API_KEY, instance.getApiKey())
+                .asString();
+    }
+
+    private boolean fail(PrintStream logger, String message) throws AbortException {
+        if (failBuild) {
+            throw new AbortException(message);
+        } else {
+            logger.println(message);
+            return true;
+        }
     }
 
     @Extension
@@ -93,6 +125,10 @@ public class ThemisRefreshNotifier extends Notifier {
                                                                  i.getName(),
                                                                  i.getName().equals(instanceName)))
                                             .collect(Collectors.toList()));
+        }
+
+        public List<ThemisInstance> getInstances() {
+            return globalConfiguration.getInstances();
         }
 
     }
